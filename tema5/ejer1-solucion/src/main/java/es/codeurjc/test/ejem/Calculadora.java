514fa825553dd84708f0678794c5b3b3d546d03e
Maes95 <michel.maes95@gmail.com>
2020-06-02 23:42:40 +0200

    Upload nexux example

---
+++ tema5/ejer1-solucion/src/main/java/es/codeurjc/test/ejem/Calculadora.java
@@ -0,0 +1,12 @@
+package es.codeurjc.test.ejem;
+
+public class Calculadora {
+
+	public int suma(int op1, int op2) {
+		return op1 + op2;
+	}
+
+	public int resta(int op1, int op2) {
+		return op1 - op2;
+	}
+}
